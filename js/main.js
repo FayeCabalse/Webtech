f6595c7ce97671749eb69f6f05e402714e6bde85
magalonglouise <35597414+magalonglouise@users.noreply.github.com>
2018-05-07 15:43:00 +0800

    added code for accordion

--- js/main.js
+++ js/main.js
@@ -176,6 +176,22 @@
 	};
 	contentWayPoint();
 
+		contentWayPoint();
+	var acc = document.getElementsByClassName("coll");
+	var i;
+
+	for (i = 0; i < acc.length; i++) {
+		acc[i].addEventListener("click", function() {
+			this.classList.toggle("active");
+
+			var panel = this.nextElementSibling;
+			if (panel.style.display === "block") {
+				panel.style.display = "none";
+			} else {
+				panel.style.display = "block";
+			}
+		});
+	}
 
 
 })(jQuery);
